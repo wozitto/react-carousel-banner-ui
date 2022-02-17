dc71b0688fd641bc4d27cf75fe7385f4eb128c45
Hirookatoshito <toshitosoccer11@gmail.com>
2022-02-17 16:07:47 +0900

    feat: bannerItem fontawsome-icon 実装

--- src/CarouselBanner/components/BannerItem.tsx
+++ src/CarouselBanner/components/BannerItem.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChartLine,
+  faNoteSticky,
+  faPrint,
+  faCalculator,
+  faIdCard,
+} from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
-  src?: string;
+  icon?: string;
   index?: number;
 };
 
 export const BannerItem = (props: Props) => {
-  return <div>{props.index}</div>;
+  const icons = [faChartLine, faNoteSticky, faPrint, faCalculator, faIdCard];
+  const iconStyle: React.CSSProperties = { padding: 10, fontSize: 50 };
+
+  return <FontAwesomeIcon icon={icons[props.index!]} style={iconStyle} />;
 };
